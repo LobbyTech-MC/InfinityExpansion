41b0d9e38acdc9870fd55c1cf783633426510a88
zimzaza4 <53421735+zimzaza4@users.noreply.github.com>
2021-01-02 18:30:28 +0800

    Update Categories.java

--- src/main/java/io/github/mooy1/infinityexpansion/lists/Categories.java
+++ src/main/java/io/github/mooy1/infinityexpansion/lists/Categories.java
@@ -19,39 +19,39 @@ public final class Categories {
     private static final InfinityExpansion instance = InfinityExpansion.getInstance();
     
     public static final MainCategory MAIN = new MainCategory(new NamespacedKey(instance, "main"), 
-            new CustomItem(Material.NETHER_STAR, "&b无限&7拓展"), -1
+            new CustomItem(Material.NETHER_STAR, "&bInfinity &7Expansion"), -1
     );
 
     public static final Category MAIN_MATERIALS = new SubCategory(new NamespacedKey(instance, "main_materials"),
-            new CustomItem(Material.NETHER_STAR, "&b无限&7拓展"), 2
+            new CustomItem(Material.NETHER_STAR, "&bInfinity &7Expansion"), 2
     );
 
     public static final Category BASIC_MACHINES = new SubCategory(new NamespacedKey(instance, "basic_machines"),
-            new CustomItem(Material.LOOM, "&9基础&7机械"), 2
+            new CustomItem(Material.LOOM, "&9Basic &7Machines"), 2
     );
 
     public static final Category ADVANCED_MACHINES = new SubCategory(new NamespacedKey(instance, "advanced_machines"),
-            new CustomItem(Material.BLAST_FURNACE, "&c高级的&7机械"), 2
+            new CustomItem(Material.BLAST_FURNACE, "&cAdvanced &7Machines"), 2
     );
 
     public static final Category STORAGE_TRANSPORT = new SubCategory(new NamespacedKey(instance, "storage_transport"),
-            new CustomItem(Material.ENDER_CHEST, "&6储存"), 2
+            new CustomItem(Material.BEEHIVE, "&6Storage"), 2
     );
     
     public static final Category MOB_SIMULATION = new SubCategory(new NamespacedKey(instance, "mob_simulation"),
-            new CustomItem(Material.SPAWNER, "&b生物模拟"), 2
+            new CustomItem(Material.BEACON, "&bMob Simulation"), 2
     );
     
     public static final Category INFINITY_MATERIALS = new SubCategory(new NamespacedKey(instance, "infinity_materials"),
-            new CustomItem(Material.NETHERITE_BLOCK, "&b无限&a材料"), 2
+            new CustomItem(Material.NETHERITE_BLOCK, "&bInfinity &aMaterials"), 2
     );
 
     public static final Category INFINITY_RECIPES = new InfinityCategory(new NamespacedKey(instance, "infinity_recipes"),
-            new CustomItem(Material.SMITHING_TABLE, "&b无限&7配方"), 2
+            new CustomItem(Material.RESPAWN_ANCHOR, "&bInfinity &7Recipes"), 2
     );
     
     public static final Category INFINITY_CHEAT = new SubCategory(new NamespacedKey(instance, "infinity_cheat"),
-            new CustomItem(Material.SMITHING_TABLE, "&b无限&7配方 &c- 不是真的配方!"), 2
+            new CustomItem(Material.RESPAWN_ANCHOR, "&bInfinity &7Recipes &c- INCORRECT RECIPE"), 2
     );
     
 }
